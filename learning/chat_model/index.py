1e31972f6a8994ac8b815e43f5014935739283d2
yashif khan <yashifyashifkhan@gmail.com>
2026-08-05 23:33:07 +0530

    learn about the output parser

--- learning/chat_model/index.py
+++ learning/chat_model/index.py
@@ -6,14 +6,14 @@ import os
 
 NVIDIA_API_KEY = os.getenv("NVIDIA_API_KEY")
 client = ChatNVIDIA(
-#   model="z-ai/glm-5.2",
-model="nvidia/nemotron-3-ultra-550b-a55b",
-#  model="nvidia/nemotron-3-super-120b-a12b",
-  api_key=NVIDIA_API_KEY , 
-  temperature=1,
-  top_p=1,
-  max_completion_tokens=16384,
-  seed=42,
+    #   model="z-ai/glm-5.2",
+    model="nvidia/nemotron-3-ultra-550b-a55b",
+    #  model="nvidia/nemotron-3-super-120b-a12b",
+    api_key=NVIDIA_API_KEY , 
+    temperature=1,
+    top_p=1,
+    max_completion_tokens=16384,
+    seed=42,
   
 )
 
